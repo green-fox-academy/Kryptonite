978cd5409c5971dd5c8c0e3b7c9f73761b76411b
Attila Fekete <feketeattila89@gmail.com>
2019-06-17 16:47:57 +0200

    d5

--- src/Future/Future.java
+++ src/Future/Future.java
@@ -2,7 +2,7 @@ package Future;
 
 public class Future {
 
-  public void future () {
-    System.out.println("Majom");
+  public void future (String paramOne, String paramTwo) {
+    System.out.println(paramOne + " " + paramTwo);
   }
 }
